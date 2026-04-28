c6730090750e8e48b61b1e26d3efe4d8bf80cc2d
Elias Knoch <elliknoch.16@gmail.com>
2026-04-28 09:09:27 +0200

    Alte auskommentierte Zeilen entfernt

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-#from fastapi import FastAPI
-
-#app = FastAPI()
-
-#@app.get("/")
-#def root():
-#    return {"message": "Hello, World!"}
-
-#@app.get("/name/{name}")
-#def greet_name(name: str):
-#    return {"message:"f"Hello, {name}!"}
-
 
 from fastapi import FastAPI
 
